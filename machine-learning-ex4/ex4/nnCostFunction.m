4b231f2c3a8245c3688a412f799de266f59b6f90
Mak2006 <gmcct.adm@gmail.com>
2020-05-26 18:53:13 +0100

    week 5

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -163,7 +163,8 @@ J = J + reg
 
 %So now we calculte grads for reg
 Theta1_reg_grad = (lambda/m) * [zeros(size(Theta1, 1), 1) Theta1(:,2:end)]; 
-
+Theta2_reg_grad = (lambda/m) * [zeros(size(Theta2, 1), 1) Theta2(:,2:end)]; 
+  
 
 
 
